f7401e927875d4035b194343214d06e87cb37f43
maneeshk3 <maneesh.kumar67@live.com>
2023-02-15 20:18:59 +0530

    Project Complete. modified:   index.html modified:   script.js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-init = funtion(){
+let saveText, tick;
+let saveTextData = "textData";
+
+function init(){
+    tick = document.getElementById("checkBox");
+    saveText = document.getElementById("textArea");
+
+    const textData = localStorage.getItem(saveTextData);
+    if(textData.length > 0){
+        saveText.value = textData;
+    }
+
+    saveText.oninput = function(){
+        if (tick.checked){
+            const currentText = saveText.value;
+            console.log("checked");
+            localStorage.setItem(saveTextData, currentText);
+                
+            }
+        }
+
+    tick.onclick = function(){
+        if(tick.checked){
+            const currentText = saveText.value;
+            localStorage.setItem(saveTextData, currentText);
+        }
+        if(tick.checked === false){
+            localStorage.setItem(saveTextData,"");
+        }
+    }
 
     
+
+
 };
 
-window.onload =inti();
+window.onload =init;
